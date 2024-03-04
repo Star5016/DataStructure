5f2202bf349c1b02439bb363c06bd131108c265b
star <lyx6686@gmail.com>
2024-03-05 07:32:30 +0800

    习题2-8

---
+++ 2-8.cpp
@@ -0,0 +1,20 @@
+void swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void Sort(SqList*& L) {
+    int i = 0, j = L->length - 1;
+    while (i < j) {
+        while (i < j && L->data[j]>0) {
+            j--;
+        }
+        while (i < j && L->data[i]<0) {
+            i++;
+        }
+        if (i < j) {
+            swap(L->data[i], L->data[j]);
+        }
+    }
+}
